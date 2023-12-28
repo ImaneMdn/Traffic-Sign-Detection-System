2596ea918ef9ddfb525739371c4498776be0b065
ImaneMdn <115882702+ImaneMdn@users.noreply.github.com>
2023-12-28 14:22:12 +0000

    added mobile app

---
+++ Mobile/babel.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+};
+// module.exports = async function(api) {
+//   return {
+//     presets: ['module:metro-react-native-babel-preset'],
+//   };
+// };
+
+
